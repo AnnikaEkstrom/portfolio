953846150cc5067b419c186cb7379abb993f7cb6
AnnikaEkstrom <ekstromannika@hotmail.com>
2018-01-15 12:07:03 +0100

    added mobile to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,6 +17,14 @@ class About extends React.Component {
                 <div className="front-logo" />
                 <div className="front-title">
                   <h1 id="about-headline">Education <br />
+                    <p id="show">
+                    I am currently studying web development at Technigo.
+                        In 12 weeks I have been learning HTML, CSS, JavaScript,
+                        React and Node. It is an intense program and every day has
+                        been filled with new information, individual assignments
+                        and group assignment. <br /> Moreover, I have a Master
+                        in Business from Linköping University.
+                    </p>
                     <i className="material-icons">school</i>
                   </h1>
                 </div>
@@ -24,7 +32,7 @@ class About extends React.Component {
               <div className="back">
                 <div className="back-logo" />
                 <div className="back-title">Education</div>
-                <p>I am currently studying web development at Technigo.
+                <p id="showflip">I am currently studying web development at Technigo.
                     In 12 weeks I have been learning HTML, CSS, JavaScript,
                     React and Node. It is an intense program and every day has
                     been filled with new information, individual assignments
@@ -42,6 +50,14 @@ class About extends React.Component {
               <div className="front">
                 <div className="front-title">
                   <h1 id="about-headline">Work <br />
+                    <p id="show">
+                    Hitachi, EF and EMG are three companies
+                    where I worked during six years in Stockholm.
+                    My areas have been Account Manager and project manager,
+                    mainly working with customer relationships, customer service,
+                    project management and marketing. I am used to work in
+                    teams and I value a humble atmosphere.
+                    </p>
                     <i className="material-icons">work</i>
                   </h1>
                 </div>
@@ -50,7 +66,7 @@ class About extends React.Component {
               <div className="back">
                 <div className="back-logo" />
                 <div className="back-title">Work experience</div>
-                <p>Hitachi, EF and EMG are three companies
+                <p id="showflip">Hitachi, EF and EMG are three companies
                 where I worked during six years in Stockholm.
                 My areas have been Account Manager and project manager,
                 mainly working with customer relationships, customer service,
@@ -70,7 +86,7 @@ class About extends React.Component {
               <div className="back">
                 <div className="back-logo" />
                 <div className="back-title">Personality</div>
-                <p>I see myself as an adventurous and
+                <p id="showflip">I see myself as an adventurous and
                   a social person who loves to meet new people.
                   I love travelling and I have seen a lot of places in the
                   world when I have been working, studying and just exploring
@@ -80,6 +96,14 @@ class About extends React.Component {
               </div>
             </div>
           </div>
+          <p id="show">
+            I see myself as an adventurous and
+              a social person who loves to meet new people.
+              I love travelling and I have seen a lot of places in the
+              world when I have been working, studying and just exploring
+              places abroad. In the summer I like to play golf and in the
+              winter I prefer skiing :)
+          </p>
         </div>
       </div>
     )
